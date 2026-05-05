f25b21c36d985f1be3f4f8f3bcbfbf002cc84af0
asmithbot <asmithbot@proton.me>
2026-05-05 11:16:02 -0700

    Improve laser renderer matching (#12118)
    * Improve pppRenderLaser matching
    
    * Improve laser trail alpha matching
    
    ---------
    
    Co-authored-by: Zachary Canann <zacharycanann@gmail.com>

--- src/pppLaser.cpp
+++ src/pppLaser.cpp
@@ -148,8 +148,8 @@ extern "C" void pppRenderLaser(struct pppLaser *pppLaser, struct pppLaserUnkB *p
     pppFMATRIX cameraMtx;
     pppFMATRIX modelMtx;
     Vec shapePos;
-    Vec debugSource;
     Vec spherePos;
+    Vec debugSource;
     _GXColor debugColor;
     _GXColor trailColor;
     _GXColor color;
@@ -261,11 +261,11 @@ extern "C" void pppRenderLaser(struct pppLaser *pppLaser, struct pppLaserUnkB *p
         trailColor.a = alphaMax;
 
         GXBegin(GX_TRIANGLES, GX_VTXFMT7, (u16)((step->m_payload[0x1e] - 1) * 3));
-        int alpha = 0;
+        u8 alpha = 0;
         for (i = 0; (int)i < (int)(step->m_payload[0x1e] - 1); i++) {
             u0 = (float)i * uvStep;
             u1 = (float)(i + 1) * uvStep;
-            trailColor.a = alphaMax - (u8)alpha;
+            trailColor.a = alphaMax - alpha;
             alpha += alphaStep;
 
             GXPosition3f32(work->m_origin.x, work->m_origin.y, work->m_origin.z);
@@ -276,7 +276,7 @@ extern "C" void pppRenderLaser(struct pppLaser *pppLaser, struct pppLaserUnkB *p
             GXColor1u32(*(u32*)&trailColor);
             GXTexCoord2f32(u0, kPppLaserZero);
 
-            trailColor.a = alphaMax - (u8)(alphaStep * (i + 1));
+            trailColor.a = alphaMax - alpha;
             GXPosition3f32(work->m_points[i + 1].x, work->m_points[i + 1].y, work->m_points[i + 1].z);
             GXColor1u32(*(u32*)&trailColor);
             GXTexCoord2f32(u1, kPppLaserZero);
