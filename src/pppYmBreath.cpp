7a40fa699a33b71927698bf187dc72db732d8d7a
BrilliantWasp <brilliantwasp@proton.me>
2026-05-06 23:51:17 -0700

    Improve Ym particle objdiff ordering (#12554)
    * Improve Ym deformation render matrix ordering
    
    * Improve Ym breath sdata constant order
    
    ---------
    
    Co-authored-by: Zachary Canann <zcanann@Zacharys-Mac-mini.local>

--- src/pppYmBreath.cpp
+++ src/pppYmBreath.cpp
@@ -896,11 +896,6 @@ void UpdateParticle(VYmBreath* vYmBreath, PYmBreath* pYmBreath, PARTICLE_DATA* p
     }
 }
 
-extern "C" const char lbl_80330CB8[] = "FFCC";
-extern "C" const char lbl_80330CC0[] = "GDS";
-extern "C" const char lbl_80330CC4[] = "GC";
-extern "C" const char lbl_80330CC8[] = "1.00";
-
 /*
  * --INFO--
  * PAL Address: 0x800c118c
@@ -1062,6 +1057,11 @@ void BirthParticle(_pppPObject*, VYmBreath* vYmBreath, PYmBreath* pYmBreath, VCo
         particleColor->m_colorFrameDeltas[3] = params->m_colorFrameDelta3;
     }
 }
+
+extern "C" const char lbl_80330CB8[] = "FFCC";
+extern "C" const char lbl_80330CC0[] = "GDS";
+extern "C" const char lbl_80330CC4[] = "GC";
+extern "C" const char lbl_80330CC8[] = "1.00";
 
 /*
  * --INFO--
